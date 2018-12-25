9ceb840430d3002f708f91d7eaf876bd0f705b06
Mei Takahashi <me.7tspr@gmail.com>
2018-12-25 17:00:09 +0900

    相関行列の計算

--- Assets/Script/ControllerMover.cs
+++ Assets/Script/ControllerMover.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class ControllerMover : MonoBehaviour {
 
-    Rigidbody m_Rigidbody;
     GameObject LaserPointer;
     LaserPointer script;
+    public float moveSpeed = 0.1f;
 
     void Start () {
-        LaserPointer = GameObject.Find("LaserPointer"); //LaserPointerをオブジェクトの名前から取得して変数に格納する
+        LaserPointer = GameObject.Find("LaserPointer"); // LaserPointerをオブジェクトの名前から取得して変数に格納する
         script = LaserPointer.GetComponent<LaserPointer>();
     }
 	
-	// Update is called once per frame
 	void Update () {
-    
+        // トリガーを押している間はコントローラーの向きに移動
         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)){ // トリガー押されたら
-            transform.position += pointerRay.position;
+            moveFoward();
         }
+    }
+
+    private void moveFoward()
+    {
+        Vector3 direction = new Vector3(LaserPointer.GetComponent<Transform>().forward.x, LaserPointer.GetComponent<Transform>().forward.y, LaserPointer.GetComponent<Transform>().forward.z).normalized * moveSpeed;
+        LaserPointer.GetComponent<Transform>().position += direction;
     }
 }
--- Assets/Script/visualization.cs
+++ Assets/Script/visualization.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿// 入力ファイルからデータをとってPCA.csに渡し、主成分分析後のデータを可視化する。
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
@@ -9,9 +10,45 @@ public class visualization : MonoBehaviour {
     TextAsset csvFile;
     List<string[]> csvDatas = new List<string[]>(); // CSVの中身を入れるリスト;
 
-    public GameObject obj;
+    public GameObject obj;//TODOVRcamera;
+    //public static int data_count;
 
-    void Start () {
+    void Start() {
+
+        //int dimension = 3; //TODO:GetComponent<PCA>().dimension;
+        //Debug.Log("dimension = " + dimension);
+
+        //csvFile = Resources.Load("data1") as TextAsset; // Resouces下のCSV読み込み。拡張子は無視
+        ////print(csvFile.text);
+        //StringReader reader = new StringReader(csvFile.text);
+
+        //// ,で分割しつつ一行ずつ読み込み
+        //// リストに追加
+        //// StringReader.Peek : 読み取り対象の次の文字を返す。
+        //// 使用できる文字がないorストリームがシークをサポートしていない場合-1となる。
+        //while (reader.Peek() != -1)
+        //{
+        //    string line = reader.ReadLine(); // 一行ずつ読み込み
+        //    csvDatas.Add(line.Split(',')); // ,区切りでリストに追加
+        //}
+       
+        //data_count = csvDatas.Count;
+
+        //float[][] multi_dim = new float[data_count][dimension];
+        //for (int i=0; i<data_count; i++)
+        //{
+        //    for (int j=0; j<dimension; j++)
+        //    {
+        //        multi_dim[i][j] = float.Parse(csvDatas[i][j]);
+        //    }
+
+        //}
+
+        //// 関数呼び出し主成分分析
+        //GetComponent<PCA>().PCAmalysis(multi_dim[data_count][dimension])
+
+        //Instantiate(obj, new Vector3(,, ), Quaternion.identity);
+
         csvFile = Resources.Load("data1") as TextAsset; // Resouces下のCSV読み込み。拡張子は無視
         //print(csvFile.text);
         StringReader reader = new StringReader(csvFile.text);
@@ -24,14 +61,9 @@ public class visualization : MonoBehaviour {
             csvDatas.Add(line.Split(',')); // ,区切りでリストに追加
         }
 
-        for (int i=0; i<csvDatas.Count(); i++)
+        for (int i = 0; i < csvDatas.Count(); i++)
         {
             Instantiate(obj, new Vector3(float.Parse(csvDatas[i][0]), float.Parse(csvDatas[i][1]), float.Parse(csvDatas[i][2])), Quaternion.identity);
         }
     }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
-}
+ }
