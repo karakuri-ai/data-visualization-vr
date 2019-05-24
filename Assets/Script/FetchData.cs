8a07e4be854e8ff292c8923f4d92411970a8ba67
Mei Takahashi <me.7tspr@gmail.com>
2019-05-24 17:26:22 +0900

    Add Assets/Script/Scaling.cs and /FetchData.cs

---
+++ Assets/Script/FetchData.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using MiniJSON;
+using System;
+
+/// <summary>
+/// GETリクエストを送信して、AWSから可視化するデータを取ってくる。
+/// Main.FetchDataにアタッチされている。
+/// </summary>
+/// 
+/// レスポンス:
+///     {
+///         "statusCode": int;
+///         "pca_data": Dictionary<string, int[]>;
+///     }
+/// 
+/// Dictionaryの具体例:
+///     {
+///         "x": [2.5, 0.1, 0.9, 15.2, 3.3, 3.7]
+///         "y": [0.6, 5.1, 8.3, 2.0, 1.1, 6.2]
+///         "z": [0.7, 1.2, 4.7, 3.1, 5.2, 2.6]
+///     }
+public class FetchData : MonoBehaviour {
+
+    public string text;
+    public List<float[]> Datas = new List<float[]>();
+    public IList x_initial, y_initial, z_initial;
+
+    public GameObject obj;
+    public GameObject camera;
+
+    public void OnClick()
+    {
+        StartCoroutine(GetText());
+
+        //text = "{\"pca_data\": {\"y\": [33, 4]}}";
+        Debug.Log(text);
+        
+        // MiniJSON
+        var json = Json.Deserialize(text) as Dictionary<string, object>;
+        //Debug.Log(json["statusCode"]);
+        //string pca_data = json["pca_data"].ToString();
+        //var x = Json.Deserialize(pca_data) as Dictionary<string, object>;
+        //Debug.Log(x);
+        var pca_data = (Dictionary<string, object>)json["pca_data"];
+        //var data = Json.Deserialize(pca_data) as Dictionary<string, object>;
+        //Debug.Log(pca_data);
+        //Debug.Log(x[0]);
+        x_initial = (IList)pca_data["x"];
+        y_initial = (IList)pca_data["y"];
+        z_initial = (IList)pca_data["z"];
+        Debug.Log(x_initial[1]);
+
+        // 可視化処理
+        for (int i = 0; i < x_initial.Count; i++)
+        {
+            //GameObject o = Instantiate(obj, new Vector3(float.Parse((string)x_initial[i]), float.Parse((string)y_initial[i]), float.Parse((string)z_initial[i])), Quaternion.identity); // うまくいかない
+            GameObject o = Instantiate(obj, new Vector3((float)(double)x_initial[i], (float)(double)y_initial[i], (float)(double)z_initial[i]), Quaternion.identity);
+            o.transform.parent = camera.transform;
+        }
+
+        //    List<float> z = (List<float>)pca_data["z"];
+        //    Debug.Log(x[0]);
+        //    // TODO:Datasの形にする必要もない？そのままx[], y[], z[]を渡してあげればいい？
+        //    // x[]をpublicにする方法はない？
+        //    for(int i = 0; i < x.Count; i++)
+        //    {
+        //        Datas[i][0] = x[i];
+        //        Datas[i][1] = y[i];
+        //        Datas[i][2] = z[i];
+        //    }
+    }
+
+    // https://blog.applibot.co.jp/2016/07/20/unity-webrequest/
+    // 使用する際はurlを変更する。
+    IEnumerator GetText()
+    {
+        UnityWebRequest request = UnityWebRequest.Get("https://u9o2yq1n5j.execute-api.ap-northeast-1.amazonaws.com/sample");
+        // 下記でも可
+        // UnityWebRequest request = new UnityWebRequest("http://example.com");
+        // methodプロパティにメソッドを渡すことで任意のメソッドを利用できるようになった
+        // request.method = UnityWebRequest.kHttpVerbGET;
+
+        // リクエスト送信
+        yield return request.SendWebRequest();
+
+        // 通信エラーチェック
+        if (request.isNetworkError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            if (request.responseCode == 200)
+            {
+                // UTF8文字列として取得する
+                text = request.downloadHandler.text;
+                //Debug.Log(text);
+
+            }
+        }
+    }
+}
---
+++ Assets/Script/Scaling.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// スケールを変更する。
+/// オブジェクトの座標と大きさを変更することで、相対的に拡大および縮小する。
+/// OVRCameraRigにアタッチされている。
+/// </summary>
+public class Scaling : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update () {
+        // タッチされた座標の取得
+        Vector2 touchPadPt = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+
+        // タッチパッドを押している時は拡大or縮小
+        if (touchPadPt.y > 0.5 && -0.5 < touchPadPt.x && touchPadPt.x < 0.5) // 上方向を押している時は拡大
+        {
+            enlarge();
+        }
+        else if (touchPadPt.y < -0.5 && -0.5 < touchPadPt.x && touchPadPt.x < 0.5) // 下方向を押している時は縮小
+        {
+            reduce();
+        }
+
+    }
+
+    private void enlarge()
+    {
+        Vector3 scale = this.transform.localScale; // プロットのスケール
+        //Vector3 centerCoordinates = this.transform.position; // カメラの座標を中心座標とする
+        //float x_new, y_new, z_new;
+        float enlarge_speed = 1.1f; // スケールをa倍する
+
+        //// x, y, zの計算
+        //float x_scale = scale.x;
+        //float y_scale = scale.y;
+        //float z_scale = scale.z;
+
+        //GameObject camera = OVRCameraRig;
+        //Transform camera = OVRCameraRig.transform;
+        // カメラの位置を取得
+        //Transform transform = GetComponent<Transform>(OVRCameraRig);
+        Vector3 new_scale = scale * enlarge_speed * Time.deltaTime;
+        this.transform.localScale = new_scale;
+    }
+
+    private void reduce()
+    {
+        Vector3 scale = this.transform.localScale; // プロットのスケール
+        float reduce_speed = 0.9f;
+        Vector3 new_scale = scale * reduce_speed * Time.deltaTime;
+        this.transform.localScale = new_scale;
+    }
+
+}
