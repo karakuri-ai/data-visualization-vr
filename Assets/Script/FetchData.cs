fe7ac86bc9b0ec778ea2b716c2797be5b20e1ff0
Mei Takahashi <me.7tspr@gmail.com>
2019-06-29 16:25:15 +0900

    Add API for fetching data from AWS

--- Assets/Script/FetchData.cs
+++ Assets/Script/FetchData.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 using MiniJSON;
 using System;
 
 /// <summary>
 /// GETリクエストを送信して、AWSから可視化するデータを取ってくる。
-/// Main.FetchDataにアタッチされている。
+/// Canvas.FetchButtonにアタッチされている。
 /// </summary>
 /// 
 /// レスポンス:
@@ -24,53 +25,23 @@ using System;
 ///     }
 public class FetchData : MonoBehaviour {
 
+
     public string text;
-    public List<float[]> Datas = new List<float[]>();
+    //public List<float[]> Datas = new List<float[]>();
     public IList x_initial, y_initial, z_initial;
 
     public GameObject obj;
     public GameObject camera;
+    public GameObject f;
+
+    //public Data visual_data = new Data();
+    //public Data visual_data;
+    //public Data visual_data = new Data();
+    public List<Vector3> data = new List<Vector3>();
 
     public void OnClick()
     {
         StartCoroutine(GetText());
-
-        //text = "{\"pca_data\": {\"y\": [33, 4]}}";
-        Debug.Log(text);
-        
-        // MiniJSON
-        var json = Json.Deserialize(text) as Dictionary<string, object>;
-        //Debug.Log(json["statusCode"]);
-        //string pca_data = json["pca_data"].ToString();
-        //var x = Json.Deserialize(pca_data) as Dictionary<string, object>;
-        //Debug.Log(x);
-        var pca_data = (Dictionary<string, object>)json["pca_data"];
-        //var data = Json.Deserialize(pca_data) as Dictionary<string, object>;
-        //Debug.Log(pca_data);
-        //Debug.Log(x[0]);
-        x_initial = (IList)pca_data["x"];
-        y_initial = (IList)pca_data["y"];
-        z_initial = (IList)pca_data["z"];
-        Debug.Log(x_initial[1]);
-
-        // 可視化処理
-        for (int i = 0; i < x_initial.Count; i++)
-        {
-            //GameObject o = Instantiate(obj, new Vector3(float.Parse((string)x_initial[i]), float.Parse((string)y_initial[i]), float.Parse((string)z_initial[i])), Quaternion.identity); // うまくいかない
-            GameObject o = Instantiate(obj, new Vector3((float)(double)x_initial[i], (float)(double)y_initial[i], (float)(double)z_initial[i]), Quaternion.identity);
-            o.transform.parent = camera.transform;
-        }
-
-        //    List<float> z = (List<float>)pca_data["z"];
-        //    Debug.Log(x[0]);
-        //    // TODO:Datasの形にする必要もない？そのままx[], y[], z[]を渡してあげればいい？
-        //    // x[]をpublicにする方法はない？
-        //    for(int i = 0; i < x.Count; i++)
-        //    {
-        //        Datas[i][0] = x[i];
-        //        Datas[i][1] = y[i];
-        //        Datas[i][2] = z[i];
-        //    }
     }
 
     // https://blog.applibot.co.jp/2016/07/20/unity-webrequest/
@@ -95,11 +66,35 @@ public class FetchData : MonoBehaviour {
         {
             if (request.responseCode == 200)
             {
-                // UTF8文字列として取得する
+                //UTF8文字列として取得する
                 text = request.downloadHandler.text;
-                //Debug.Log(text);
-
+                Debug.Log(text);
+                BodyJson bodyJson = JsonUtility.FromJson<BodyJson>(text);
+                List<float> x = bodyJson.pca_data.x;
+                List<float> y = bodyJson.pca_data.y;
+                List<float> z = bodyJson.pca_data.z;
+                for (int i = 0; i < x.Count; i++)
+                {
+                    data.Add(new Vector3((float)x[i], (float)y[i], (float)z[i]));
+                }
+                Debug.Log(data[0]);
             }
         }
+    }
+
+    // AWS から取得するデータの形式を規定するクラス
+    [System.Serializable]
+    class BodyJson
+    {
+        public int statusCode;
+        public PosJson pca_data;
+    }
+
+    [System.Serializable]
+    class PosJson
+    {
+        public List<float> x;
+        public List<float> y;
+        public List<float> z;
     }
 }
--- Assets/Script/visualization.cs
+++ Assets/Script/visualization.cs
@@ -1,37 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
 using System.Linq;
 
 /// <summary>
-/// 主成分分析後の位置データを受け取って、プロットする。
-/// Visualizerにアタッチされている。
+/// FetchDataで取ってきた主成分分析後の位置データを受け取って、プロットする。
+/// Main.Visualizerにアタッチされている。
 /// </summary>
 public class visualization : MonoBehaviour {
 
-    TextAsset csvFile;
-    List<string[]> csvDatas = new List<string[]>(); // CSVの中身を入れるリスト;
+    // csvファイルを使う時
+    //TextAsset csvFile;
+    //List<string[]> csvDatas = new List<string[]>(); // CSVの中身を入れるリスト;
+
+    List<Vector3> data;
+    GameObject FetchButton;
 
     public GameObject obj;
+    public GameObject camera;
 
-    void Start() {
-        csvFile = Resources.Load("data1") as TextAsset; // Resouces下のCSV読み込み。拡張子は無視
-        //print(csvFile.text);
-        StringReader reader = new StringReader(csvFile.text);
+    public void OnClick()
+    {
+        // VisualizeButtonを押すと可視化処理が始まる
+        // inspectatorの操作ができない
 
-        // ,で分割しつつ一行ずつ読み込み
-        // リストに追加
-        while (reader.Peek() != -1)
+        FetchButton = GameObject.Find("Canvas/FetchButton");
+        data = FetchButton.GetComponent<FetchData>().data;
+        Debug.Log(data[0]);
+
+
+
+        //// csvファイルを読みこむ時
+        //// TODO: csvファイルを使いたいときに動かせるようにしたい(条件分岐か何かで)
+        //csvFile = Resources.Load("data1") as TextAsset; // Resouces下のCSV読み込み。拡張子は無視
+        ////print(csvFile.text);
+        //StringReader reader = new StringReader(csvFile.text);
+
+        //// ,で分割しつつ一行ずつ読み込み
+        //// リストに追加
+        //while (reader.Peek() != -1)
+        //{
+        //    string line = reader.ReadLine(); // 一行ずつ読み込み
+        //    csvDatas.Add(line.Split(',')); // ,区切りでリストに追加
+        //}
+        ////Debug.Log(script.x_initial[0]);
+
+        //// 可視化処理
+        //for (int i = 0; i < csvDatas.Count(); i++)
+        //{
+        //    //Instantiate(obj, new Vector3((float)script.x_initial[i], (float)script.y_initial[i], (float)script.z_initial[i]), Quaternion.identity);
+        //    GameObject o = Instantiate(obj, new Vector3(float.Parse(csvDatas[i][0]), float.Parse(csvDatas[i][0]), float.Parse(csvDatas[i][0])), Quaternion.identity);
+        //    o.transform.parent = camera.transform;
+        //}
+
+
+        //可視化処理
+        for (int i = 0; i < data.Count(); i++)
         {
-            string line = reader.ReadLine(); // 一行ずつ読み込み
-            csvDatas.Add(line.Split(',')); // ,区切りでリストに追加
-        }
-
-        // 可視化処理
-        for (int i = 0; i < csvDatas.Count(); i++)
-        {
-            Instantiate(obj, new Vector3(float.Parse(csvDatas[i][0]), float.Parse(csvDatas[i][1]), float.Parse(csvDatas[i][2])), Quaternion.identity);
+            GameObject o = Instantiate(obj, data[i], Quaternion.identity);
+            o.transform.parent = camera.transform;
         }
     }
  }
